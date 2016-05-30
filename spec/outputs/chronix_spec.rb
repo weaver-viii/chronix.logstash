6166b4a010ebb6172edef65ab2af246110a58333
Max Jalowski <max.jalowski@fau.de>
2016-05-30 22:34:54 +0200

    fixed spec

--- spec/outputs/chronix_spec.rb
+++ spec/outputs/chronix_spec.rb
@@ -77,7 +77,7 @@ describe LogStash::Outputs::Chronix do
       points.p << subject.createChronixPoint(ttimestamp, tvalue)
       phash = {"startTime" => ttimestamp, "lastTimestamp" => ttimestamp, "points" => points}
       document = subject.createSolrDocument(tmetric, phash)
-      sampleDoc = { :metric => tmetric, :start => phash["startTime"], :end => phash["lastTimestamp"], :data => "H4sIAAAAAAAA/+Pi59jx9v12VkEGMFB1AACWVOXHEQAAAA==", :threshold => 10 }
+      sampleDoc = { :metric => tmetric, :start => phash["startTime"], :end => phash["lastTimestamp"], :data => "H4sIAAAAAAAA/+Pi59jx9v12VkEGMFB1AACWVOXHEQAAAA==" }
       expect(document).to eq(sampleDoc)
     end
 
